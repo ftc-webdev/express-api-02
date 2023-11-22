b06d6fac751333e399d97b20d154e519b0d25e26
eamole <eamole@hotmail.com>
2023-11-22 13:23:38 +0000

    This is a "fork" of express-api-01 Added a models folder, will now map routes to models to server data via our API
    Signed-off-by: eamole <eamole@hotmail.com>

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,5 @@
 import users from './users.js'
 
-
 export default (app) => {
   app.use('/api/v1/users', users);
 }
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,9 +1,18 @@
 import express from 'express'
 const router = express.Router();
 
+const users = [
+  {name: 'Timmy'},
+  {name: 'Tom'},
+  {name: 'Dick'},
+  {name: 'Harriet'},
+  {name: 'Mary'},
+] 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.json({users: [{name: 'Timmy'}]});
+  res.json({
+    users 
+  });
 });
 
 export default router
