6294d056b3eb03f74ee5b2cb022dbdc01d784752
eamole <eamole@hotmail.com>
2023-11-30 12:44:42 +0000

    Added our static front end App to the public folder of the server Modified the server app to serve the public folder
    Signed-off-by: eamole <eamole@hotmail.com>

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from 'express'
-// import path from 'path'
+import path from 'path'
+import { fileURLToPath } from 'url' // ES5 problem with __dirname
 import cookieParser from 'cookie-parser'
 import bodyParser from 'body-parser'
 import cors from 'cors'
@@ -8,13 +9,25 @@ import routes from './routes/index.js'
 
 var app = express();
 
-// app.use(bodyParser.json());
+// path to get around problem of __dirname not being available in ES5 modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// app.use(express.static("public"))
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser())
 app.use(cors())
 
 routes(app)
+
+const urlPath = path.join(__dirname, '..', 'public')
+
+app.use(express.static(urlPath));
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(urlPath, 'index.html'));
+})
 
 
 export default app
